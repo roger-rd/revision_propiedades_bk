8768ab473f8b44fdbd4a645511b92bb0af346a8f
Roger Rodriguez <rogerdavid.rd@gmail.com>
2025-08-16 18:26:54 -0400

    prueba de bd

--- index.js
+++ index.js
@@ -76,7 +76,6 @@ router.get('/db/ping', async (req, res) => {
 
 app.use('/api', routes);
 
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
