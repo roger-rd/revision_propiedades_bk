7aa917458b46580b8d95df6ecb0b4316192fe3ee
Roger Rodriguez <rogerdavid.rd@gmail.com>
2025-08-16 15:19:27 -0400

    modifica coni

--- config/bd_revision_casa.js
+++ config/bd_revision_casa.js
@@ -16,20 +16,17 @@
 // config/bd_revision_casa.js
 const { Pool } = require('pg');
 
-const isDbUrl = !!process.env.DATABASE_URL;
-const pool = new Pool(
-  isDbUrl
-    ? {
-        connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false }, // Railway Postgres
-      }
-    : {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 5432,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-      }
-);
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT || 5432,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    });
 
 module.exports = pool;
